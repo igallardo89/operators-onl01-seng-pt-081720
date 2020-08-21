b0ed145b05df2ee336a0e7a1d621939cb6d7adac
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-21 23:23:10 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,11 +3,6 @@ def unsafe?(speedle)
   
   speedle < 50 ? 35 < 40 : 60 < 90
   
-  
-
-
-
-
 
 end
 
@@ -16,9 +11,6 @@ end
 def not_safe?(speed)
   
   speed > 60 ?  40 <  60 : 60 < 90
-  
-  
-  
   
 	
 end
